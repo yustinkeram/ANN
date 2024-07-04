6446d691fd8415b549b8f47d4a5455c27f9e5f3c
yustinkeram <yustinhendrite@gmail.com>
2024-07-05 00:39:17 +0700

    Update stream-water_quality.py

--- stream-water_quality.py
+++ stream-water_quality.py
@@ -2,9 +2,10 @@ import pickle
 import streamlit as st
 import numpy as np
 
-# Memuat model yang sudah dilatih sebelumnya
-model_path = "water_quality_model.sav"
-water_quality_model = joblib.load(model_path)
+#mambaca model
+# Memuat model yang disimpan
+filename = 'water_quality.sav'
+loaded_model = pickle.load(filename)
 
 
 
